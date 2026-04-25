82832300884d46e75aa7c3b0f1b9c4c717b13de0
wyvern <nitrojector@takina.io>
2026-04-24 23:38:01 -0700

    feat: window content force rectTransform

--- Assets/Scripts/Desktop/Window/WindowContent.cs
+++ Assets/Scripts/Desktop/Window/WindowContent.cs
@@ -1,9 +1,60 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using Utility;
 
 namespace Desktop.Window
 {
-	public abstract class WindowContent : MonoBehaviour
+	[ExecuteAlways]
+	public abstract class WindowContent : MonoBehaviour, ILayoutController
 	{
+		
+		private DrivenRectTransformTracker tracker;
+		private RectTransform rt;
+
+		protected RectTransform RectTransform => rt ??= GetComponent<RectTransform>();
+
+		private void OnEnable()
+		{
+			tracker.Add(this, RectTransform,
+				DrivenTransformProperties.Anchors |
+				DrivenTransformProperties.AnchoredPosition |
+				DrivenTransformProperties.SizeDelta |
+				DrivenTransformProperties.Pivot);
+			SetLayoutHorizontal();
+			SetLayoutVertical();
+		}
+
+		private void OnDisable()
+		{
+			tracker.Clear();
+		}
+		
+#if UNITY_EDITOR
+		protected void OnValidate()
+		{
+			if (RectTransform == null) return;
+			SetLayoutHorizontal();
+			SetLayoutVertical();
+			Logr.Info($"Validated layout for {name}");
+		}
+#endif
+
+		public void SetLayoutHorizontal()
+		{
+			RectTransform.anchorMin = new Vector2(0f, RectTransform.anchorMin.y);
+			RectTransform.anchorMax = new Vector2(1f, RectTransform.anchorMax.y);
+			RectTransform.offsetMin = new Vector2(0f, RectTransform.offsetMin.y);
+			RectTransform.offsetMax = new Vector2(0f, RectTransform.offsetMax.y);
+		}
+
+		public void SetLayoutVertical()
+		{
+			RectTransform.anchorMin = new Vector2(RectTransform.anchorMin.x, 0f);
+			RectTransform.anchorMax = new Vector2(RectTransform.anchorMax.x, 1f);
+			RectTransform.offsetMin = new Vector2(RectTransform.offsetMin.x, 0f);
+			RectTransform.offsetMax = new Vector2(RectTransform.offsetMax.x, 0f);
+		}
+
 		/// <summary>
 		/// Called when the window is initialized with this content.
 		/// </summary>
