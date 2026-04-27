7ec2aca1279370c9efabb678851ba39332e2cbdd
wyvern <nitrojector@takina.io>
2026-04-26 17:20:56 -0700

    feat: gameactions structure

---
+++ Assets/Scripts/GameActions.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class GameActions : MonoBehaviour
+{
+	public static GameActions Instance { get; private set; }
+		
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+	}
+
+	public void OpenProgressionPrint()
+	{
+			
+	}
+
+	public void OpenFreePrint()
+	{
+			
+	}
+
+	public void OpenGallery()
+	{
+			
+	}
+
+	public void OpenSettings()
+	{
+		
+	}
+
+	public void ExitGame()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit();
+#endif
+	}
+}
