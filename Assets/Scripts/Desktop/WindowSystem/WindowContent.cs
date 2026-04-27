68b3e95ab41d2998eb370b30d17688fd0f9bcd2c
wyvern <nitrojector@takina.io>
2026-04-27 00:05:28 -0700

    feat: window sprite update; reference window

--- Assets/Scripts/Desktop/WindowSystem/WindowContent.cs
+++ Assets/Scripts/Desktop/WindowSystem/WindowContent.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using Utility;
 
 namespace Desktop.WindowSystem
 {
--- Assets/Scripts/WindowContents/PrinterReferenceWindowContent.cs
+++ Assets/Scripts/WindowContents/PrinterReferenceWindowContent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Desktop.WindowSystem;
+using Printer;
 using UnityEngine;
 using Utility;
 
@@ -7,10 +8,24 @@ namespace WindowContents
 {
 	public class PrinterReferenceWindowContent : WindowContent
 	{
+		public override string WindowTitle => "Reference";
+		
+		[SerializeField] public PrinterReference printerReference;
+		
 		private void Start()
 		{
 			AttachedWindow.SetPositionNormalized(new Vector2(0.75f, 0.5f), new Vector2(0.5f, 0.5f));
 			Logr.Info("Printer Reference Window Initialized");
+		}
+
+		public void SetReferenceSprite(Sprite reference)
+		{
+			if (printerReference == null)
+			{
+				return;
+			}
+			
+			printerReference.ReferenceImage.sprite = reference;
 		}
 	}
 }
